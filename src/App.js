699fef6f4158aa9e78af3792956ae5888865684b
Adam Smith <coloradoasmith@gmail.com>
2020-07-09 10:37:48 -0600

    roulette game working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Bomb from "./state-drills/Bomb";
+import RouletteGun from "./state-drills/RouletteGun";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Bomb />
+        <RouletteGun />
       </div>
     );
   }
