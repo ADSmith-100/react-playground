8de9366eb2e5743297c4de76a09a070a29d041df
Adam Smith <coloradoasmith@gmail.com>
2020-07-09 10:03:16 -0600

    Bomb app working : )

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import HelloWorld from "./state-drills/HelloWorld";
+import Bomb from "./state-drills/Bomb";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HelloWorld />
+        <Bomb />
       </div>
     );
   }
