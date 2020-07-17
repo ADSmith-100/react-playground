319d1d6569ba48aebd8c2c0a6550f176f53be69c
Adam Smith <coloradoasmith@gmail.com>
2020-07-17 12:55:18 -0600

    demonym app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import Accordian from "./state-drills/Accordian";
 
-const sections = [
-  {
-    title: "Section 1",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  },
-  {
-    title: "Section 2",
-    content:
-      "Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!",
-  },
-  {
-    title: "Section 3",
-    content:
-      "Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?",
-  },
-];
+import DemonynApp from "./demonymapp/demonymApp";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Accordian sections={sections} />
+        <DemonynApp />
       </div>
     );
   }
